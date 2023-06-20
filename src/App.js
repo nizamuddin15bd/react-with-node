7c097f126c1715bbae20dfd9c8eebb19dbda8459
Naem-Nizam <nizamuddin15bd@gmail.com>
2023-06-20 18:34:23 +0600

    Post backend a patanor neyom

--- src/App.js
+++ src/App.js
@@ -9,9 +9,37 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    // post data to server
+    fetch("http://localhost:5000/user", {
+      method: "post",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
       <h2> users id length: {users.length}</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
       <h3>
         {users.map((user) => (
           <li key={user.id}>
